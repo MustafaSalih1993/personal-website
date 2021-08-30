5ed239392cd625484fffdaa5152db0183801bd97
Mustafa <mustafasalih1991@gmail.com>
2021-08-30 16:36:24 +0300

    updated

--- src/components/body/Navbar/Navbar.js
+++ src/components/body/Navbar/Navbar.js
@@ -1,18 +1,27 @@
 import "./Navbar.css";
 import { Link } from "react-router-dom";
-function Navbar(props) {
+import { useTrail, animated, config } from "react-spring";
+
+const navLinks = [
+  { name: "PROJECTS", to: "/projects" },
+  { name: "ABOUT", to: "/about" },
+  { name: "CONTACT", to: "/contact" },
+];
+
+function Navbar() {
+  const trails = useTrail(navLinks.length, {
+    config: config.gentle,
+    from: { opacity: 0, transform: "translate3d(-100%,0,0)" },
+    to: { opacity: 1, transform: "translate3d(0,0,0)" },
+  });
   return (
     <nav className="nav-container">
       <ul>
-        <li>
-          <Link to="/projects">PROJECTS</Link>
-        </li>
-        <li>
-          <Link to="/about">ABOUT</Link>
-        </li>
-        <li>
-          <Link to="/contact">CONTACT</Link>
-        </li>
+        {trails.map((styles, i) => (
+          <animated.li style={styles} key={i}>
+            <Link to={navLinks[i].to}>{navLinks[i].name}</Link>
+          </animated.li>
+        ))}
       </ul>
     </nav>
   );
